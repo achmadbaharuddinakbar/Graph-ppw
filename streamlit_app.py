2aeb8c2b1b185cf55dde087ea2455de0ac907186
Achmad Baharuddin Akbar <112038717+achmadbaharuddinakbar@users.noreply.github.com>
2024-11-07 01:19:08 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,11 +22,11 @@ def remove_html(text):
 
 def remove_emoji(text):
     emoji_pattern = re.compile("[" 
-                               u"\U0001F600-\U0001F64F" 
-                               u"\U0001F300-\U0001F5FF" 
-                               u"\U0001F680-\U0001F6FF" 
-                               u"\U0001F1E0-\U0001F1FF" 
-                               "]+", flags=re.UNICODE)
+        u"\U0001F600-\U0001F64F" 
+        u"\U0001F300-\U0001F5FF" 
+        u"\U0001F680-\U0001F6FF" 
+        u"\U0001F1E0-\U0001F1FF" 
+        "]+", flags=re.UNICODE)
     return emoji_pattern.sub(r'', text)
 
 def remove_numbers(text):
